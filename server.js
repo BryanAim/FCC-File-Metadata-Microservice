04d47940024d6f4b9c3e60075ed35e5b708bd7dc
Isale <isale.bryan@gmail.com>
2020-06-13 10:32:17 +0300

    set up multer storage

--- server.js
+++ server.js
@@ -6,10 +6,26 @@ var cors = require('cors');
 // require and use "multer"...
 var multer = require('multer');
 
+// Set Multer storage
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, fike, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+var upload = multer({storage: storage})
+
+
+// create express app
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
